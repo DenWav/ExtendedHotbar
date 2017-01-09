d29e8c0d00eb02b91b5488f8ea11a199b4cf2256
DemonWav <demonwav@gmail.com>
2017-01-09 07:28:30 -0600

    Implement UI elements

--- src/main/java/com/demonwav/extendedhotbar/LiteModExtendedHotbar.java
+++ src/main/java/com/demonwav/extendedhotbar/LiteModExtendedHotbar.java
@@ -1,10 +1,16 @@
 package com.demonwav.extendedhotbar;
 
-import com.mumfrey.liteloader.LiteMod;
+import com.mumfrey.liteloader.Tickable;
+import com.mumfrey.liteloader.core.LiteLoader;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import org.lwjgl.input.Keyboard;
 
 import java.io.File;
 
-public class LiteModExtendedHotbar implements LiteMod {
+public class LiteModExtendedHotbar implements Tickable {
+
+    private static final KeyBinding swapKeyBinding = new KeyBinding("key.hotbar.toggle", Keyboard.KEY_R, "key.categories.litemods");
 
     @Override
     public String getName() {
@@ -18,9 +24,21 @@ public class LiteModExtendedHotbar implements LiteMod {
 
     @Override
     public void init(File configPath) {
+        LiteLoader.getInput().registerKeyBinding(swapKeyBinding);
     }
 
     @Override
     public void upgradeSettings(String version, File configPath, File oldConfigPath) {
+    }
+
+    @Override
+    public void onTick(Minecraft minecraft, float partialTicks, boolean inGame, boolean clock) {
+        if (!inGame || minecraft.currentScreen != null || !Minecraft.isGuiEnabled()) {
+            return;
+        }
+
+        if (swapKeyBinding.isPressed()) {
+
+        }
     }
 }
--- src/main/java/com/demonwav/extendedhotbar/mixin/MixinGuiInGame.java
+++ src/main/java/com/demonwav/extendedhotbar/mixin/MixinGuiInGame.java
@@ -1,15 +1,79 @@
 package com.demonwav.extendedhotbar.mixin;
 
+import com.mumfrey.liteloader.gl.GL;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiIngame;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 
 @Mixin(GuiIngame.class)
-public class MixinGuiInGame extends Gui {
+public abstract class MixinGuiInGame extends Gui {
 
-    @Overwrite
-    protected void renderHotbar(ScaledResolution sr, float partialTicks) {
+    private static final int distance = 22;
+
+    @Shadow @Final private static ResourceLocation WIDGETS_TEX_PATH;
+
+    @Shadow @Final private Minecraft mc;
+    @Shadow protected abstract void renderHotbarItem(int p_184044_1_, int p_184044_2_, float p_184044_3_, EntityPlayer player, ItemStack stack);
+
+    @Inject(method = "renderHotbar", at = @At("RETURN"))
+    public void drawTopHotbar(ScaledResolution sr, float partialTicks, CallbackInfo info) {
+        final EntityPlayer entityplayer = (EntityPlayer)this.mc.getRenderViewEntity();
+        if (entityplayer == null) {
+            return;
+        }
+
+        final int i = sr.getScaledWidth() / 2;
+
+        GL.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        mc.getTextureManager().bindTexture(WIDGETS_TEX_PATH);
+        GL.glEnableBlend();
+        drawTexturedModalRect(i - 91, sr.getScaledHeight() - 22 - distance, 0, 0, 182, 22);
+
+        GL.glEnableRescaleNormal();
+        GL.glEnableBlend();
+        GL.glBlendFuncSeparate(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA, GL.GL_ONE, GL.GL_ZERO);
+        RenderHelper.enableGUIStandardItemLighting();
+
+        for (int l = 0; l < 9; ++l) {
+            int i1 = i - 90 + l * 20 + 2;
+            int j1 = sr.getScaledHeight() - 16 - 3 - distance;
+            this.renderHotbarItem(i1, j1, partialTicks, entityplayer, entityplayer.inventory.mainInventory.get(l + 27));
+        }
+
+        RenderHelper.disableStandardItemLighting();
+        GL.glDisableRescaleNormal();
+        GL.glDisableBlend();
+    }
+
+    @Inject(method = "renderPlayerStats", at = @At("HEAD"))
+    public void movePlayerStatsUp(ScaledResolution scaledRes, CallbackInfo info) {
+        GL.glTranslated(0, -distance, 0);
+    }
+
+    @Inject(method = "renderPlayerStats", at = @At("RETURN"))
+    public void resetPlayerStats(ScaledResolution scaledRes, CallbackInfo info) {
+        GL.glTranslated(0, distance, 0);
+    }
+
+    @Inject(method = "renderExpBar", at = @At("HEAD"))
+    public void moveExpBarUp(ScaledResolution scaledRes, int x, CallbackInfo info) {
+        GL.glTranslated(0, -distance, 0);
+    }
+
+    @Inject(method = "renderExpBar", at = @At("RETURN"))
+    public void resetExpBar(ScaledResolution scaledRes, int x, CallbackInfo info) {
+        GL.glTranslated(0, distance, 0);
     }
 }
